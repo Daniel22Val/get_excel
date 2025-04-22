abe21b4dfe46f5cdaaaece492f85cc926ccb4846
Daniel-Val <daniel@val.id>
2025-04-22 08:38:12 +0700

    update last sheet

--- src/main/java/io/lacak/clone/live/shared/summary/SummaryService.java
+++ src/main/java/io/lacak/clone/live/shared/summary/SummaryService.java
@@ -5,12 +5,14 @@ import io.lacak.clone.live.io.entity.TrackSummaryEntity;
 import io.lacak.clone.live.io.repository.TrackDetailRepository;
 import io.lacak.clone.live.io.repository.TrackSummaryRepository;
 import io.lacak.clone.live.pattern.FieldConvertExcel;
+import io.lacak.clone.live.ui.model.BaseFieldResponse;
 import io.lacak.clone.live.ui.model.request.SummarySearchRequest;
 import io.lacak.clone.live.ui.model.response.SummaryResponse;
 import io.lacak.clone.live.ui.model.response.TrackListResponse;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -53,15 +55,68 @@ public class SummaryService {
         XSSFWorkbook workbook = fieldConvert.exportReport(responseList);
 
         FieldConvertExcel<TrackListResponse> fieldConvertDetail = new FieldConvertExcel<>(new TrackListResponse());
+        FieldConvertExcel<BaseFieldResponse> fieldVertical = new FieldConvertExcel<>(new BaseFieldResponse());
         for (TrackSummaryEntity entity : entityList) {
             String nopol = entity.getNopol();
             List<TrackDetailEntity> detailList = trackDetailRepository.findBySummary(entity);
             List<TrackListResponse> detailResponse = detailList.stream().map(this::toDetailResponse).toList();
             fieldConvertDetail.exportReportAnotherSheet(workbook, nopol, detailResponse);
+
+            String nopol2 = nopol + " - 2";
+            List<BaseFieldResponse> summaryNopol = getSummaryBaseField(entity);
+            fieldVertical.exportReportAnotherSheet(workbook, nopol2, summaryNopol);
         }
 
 
         return workbook;
+    }
+
+    private List<BaseFieldResponse> getSummaryBaseField (TrackSummaryEntity entity) {
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        List<BaseFieldResponse> summaryNopol = new ArrayList<>();
+        BaseFieldResponse field1  = new BaseFieldResponse();
+        field1.setSummary("Date");
+        field1.setValue(entity.getTrackDate().format(format));
+        summaryNopol.add(field1);
+
+        BaseFieldResponse field2 = new BaseFieldResponse();
+        field2.setSummary("Trips");
+        field2.setValue(String.valueOf(entity.getTrips()));
+        summaryNopol.add(field2);
+
+        BaseFieldResponse field3 = new BaseFieldResponse();
+        field3.setSummary("Total Trip Length, km");
+        field3.setValue(String.valueOf(entity.getTotal_trip_length()));
+        summaryNopol.add(field3);
+
+        BaseFieldResponse field4 = new BaseFieldResponse();
+        field4.setSummary("Travel Time");
+        field4.setValue(entity.getTravel_time());
+        summaryNopol.add(field4);
+
+        BaseFieldResponse field5 = new BaseFieldResponse();
+        field5.setSummary("Average speed, km/h");
+        field5.setValue(String.valueOf(entity.getAvg_speed()));
+        summaryNopol.add(field5);
+
+        BaseFieldResponse field6 = new BaseFieldResponse();
+        field6.setSummary("Max speed, km/h");
+        field6.setValue(String.valueOf(entity.getMax_speed()));
+        summaryNopol.add(field6);
+
+        BaseFieldResponse field7 = new BaseFieldResponse();
+        field7.setSummary("Stop Duration");
+        field7.setValue(entity.getStop_duration());
+        summaryNopol.add(field7);
+
+        BaseFieldResponse field8 = new BaseFieldResponse();
+        field8.setSummary("Odometer value, km");
+        field8.setValue("0.1");
+        summaryNopol.add(field8);
+
+        return summaryNopol;
     }
 
     private TrackListResponse toDetailResponse (TrackDetailEntity detail) {
---
+++ src/main/java/io/lacak/clone/live/ui/model/BaseFieldResponse.java
@@ -0,0 +1,9 @@
+package io.lacak.clone.live.ui.model;
+
+import lombok.Data;
+
+@Data
+public class BaseFieldResponse extends FieldBase {
+    public String summary;
+    public String value;
+}
